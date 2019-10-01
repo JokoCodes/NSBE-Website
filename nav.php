6ac619a366cdb990c75b7606467935a46653eacc
JokoCodes <55161770+JokoCodes@users.noreply.github.com>
2019-09-30 21:12:49 -0400

    redesign by TT
    redesign by TT

--- nav.php
+++ nav.php
@@ -1,38 +1,35 @@
 <body>
 	
 	  <!-- Navigation Links -->
-	<script>
-		function showHide() {
-			var x = document.getElementById("navBar");
-			if (x.className.indexOf("w3-show") == -1) {
-				x.className += " w3-show";
-			} else {
-				x.className = x.className.replace(" w3-show", "https://lh3.googleusercontent.com/y5YBpO4lpWmABjXMcrzJP8MKDHJ6WJ230Au78kNNhFx5uj8kjQbx7sWQgkdrH-IFSlrxgA=s161");
-			}
-		}
-	</script>
-	
-	<!-- Navigation Bar (Full-Size Computer) -->
-	<nav class="w3-bar w3-black">
-		<button class="w3-button w3-hover-black w3-padding-small w3-hide-large w3-left-align w3-top" href="javascript:void(0)" onclick="showHide()" title="Toggle Navigation Menu"><img id="nsbe-uc-logo" src="https://pbs.twimg.com/media/D_sy4pgW4AAl0uO?format=jpg&name=small"/><span> </span><span> </span><i class="fa fa-bars"></i></button>
-		<a href="index.php"><img id="nsbe-uc-logo-s" src="https://lh3.googleusercontent.com/y5YBpO4lpWmABjXMcrzJP8MKDHJ6WJ230Au78kNNhFx5uj8kjQbx7sWQgkdrH-IFSlrxgA=s161" class="w3-bar-item w3-left-align w3-padding-large w3-hide-large w3-hide-medium" height="85" width="42"/></a>
-		<a href="index.php"><img id="nsbe-uc-logo-l" src="https://lh3.googleusercontent.com/y5YBpO4lpWmABjXMcrzJP8MKDHJ6WJ230Au78kNNhFx5uj8kjQbx7sWQgkdrH-IFSlrxgA=s161" class="w3-bar-item w3-left-align w3-padding-large w3-hide-small" height="85" width="42"/></a>
-    		<a href="index.php" class="w3-hover-red w3-bar-item w3-button w3-padding-large w3-cell-middle w3-hide-small w3-right-align" height="85" width="42">Home</a>
-    		<a href="about.php" class="w3-hover-yellow w3-bar-item w3-button w3-padding-large w3-cell-middle w3-hide-small w3-right-align" height="85" width="42">About Us</a>
-    		<a href="ourchapter.php" class="w3-hover-red w3-bar-item w3-button w3-padding-large w3-cell-middle w3-hide-small w3-right-align" height="85" width="42">Our Chapter</a>
-    		<a href="events.php" class="w3-hover-red w3-bar-item w3-button w3-padding-large w3-cell-middle w3-hide-small w3-right-align" height="85" width="42">Events</a>
-    		<a href="forms.php" class="w3-hover-red w3-bar-item w3-button w3-padding-large w3-cell-middle w3-hide-small w3-right-align" height="85" width="42">Forms</a>
-			<a href="contact.php" class="w3-hover-red w3-bar-item w3-button w3-padding-large w3-cell-middle w3-hide-small w3-right-align" height="85" width="42">Contact Us</a>
-    
- 	<!-- Navigation Bar (Smartphone/Tablet) -->
-	<div id="navBar" class="w3-bar-block w3-black w3-hide w3-hide-small w3-hide-medium w3-right w3-top" style="margin-top:58px; width: 100%">
-		<a href="index.php" class="w3-hover-red w3-bar-item w3-hover-light-gray w3-mobile w3-right-align">Home</a>
-		<a href="about.php" class="w3-hover-yellow w3-bar-item w3-hover-light-gray w3-mobile w3-right-align">About Us</a>
-		<a href="ourchapter.php" class="w3-hover-red w3-bar-item w3-hover-light-gray w3-mobile w3-right-align">Our Chapter</a>
-		<a href="events.php" class="w3-hover-red w3-bar-item w3-hover-light-gray w3-mobile w3-right-align">Events</a>
-		<a href="forms.php" class="w3-hover-red w3-bar-item w3-hover-light-gray w3-mobile w3-right-align">Forms</a>
-		<a href="contact.php" class="w3-hover-red w3-bar-item w3-button w3-padding-large w3-cell-middle w3-hide-small w3-right-align" height="85" width="42">Contact Us</a>
-	</div>
+    <script>
+      function showHide() {
+ var x = document.getElementById("navBar");
+ if (x.className.indexOf("w3-show") == -1) {
+ x.className += " w3-show";
+ } else {
+ x.className = x.className.replace(" w3-show", "https://lh3.googleusercontent.com/y5YBpO4lpWmABjXMcrzJP8MKDHJ6WJ230Au78kNNhFx5uj8kjQbx7sWQgkdrH-IFSlrxgA=s161");
+ }
+ }
+ </script>
+
+ <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000; box-shadow: 50px #000000">
+     
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="nav-link w3-hover-text-red" href="#" style="color: #ffffff; margin: 0px 50px 0px 150px; font-size: 20px">Home<span class="sr-only">(current)</span></a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle w3-hover-text-yellow" href="#" id="navbarDropdown" style="color: #ffffff; margin: 0px 100px 20px 100px; font-size: 20px" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Us</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="margin: 0px 100px 0px 90px">
+              <a class="dropdown-item w3-hover-yellow" href="">The Organization</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item w3-hover-red" href="">Meet the E-Board</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item w3-hover-red" href="">Our Sponsors</a>
+            </div>
+        </li>
+        <li><a class="navbar-brand" href="index.php"><img src="https://pbs.twimg.com/media/D_sy4pgW4AAl0uO.jpg" width="160" height="90" alt=""></a></li>
+        <a class="nav-link w3-hover-text-red" href="#" style="color: #ffffff; margin: 0px 100px 0px 100px; font-size: 20px">Forms</a>
+        <a class="nav-link w3-hover-text-red" href="#" style="color: #ffffff; margin: 0px 100px 0px 100px; font-size: 20px">Contact Us</a>
+      </div>
     </nav>
 	
 </body>
