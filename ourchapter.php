f2c99faa3a71b561e7d3284aae617175488e0cbf
troymead <tspringmeadows@gmail.com>
2019-10-01 18:50:51 -0400

    added mailto links

--- ourchapter.php
+++ ourchapter.php
@@ -42,7 +42,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Moriah Henry</header></li>
                 				<li><b>Year:</b> Junior</li>
                 				<li><b>Major:</b> Electrical Engineering (Robotics Minor)</li>
-								<li><b>Email:</b> president.ucnsbe@gmail.com</li>
+								<li><b>Email:</b><a href="mailto:president.ucnsbe@gmail.com"> president.ucnsbe@gmail.com</a></li>
                 				<li style="overflow-y: auto; width: 325px; height: 105px"><b>About: </b> From Lima, OH. Hobbies: Binge watching TV, playing with my dog, dancing for fun, and traveling (to try new foods). Fun fact: I’m going to be traveling to New Zealand in early 2020!   I plan to make sure the entire  E-board has all all of the resources they need to succeed and make this a great year for our chapter!</li>
                 				<li><footer><p style="font-weight: 600; text-align: center; color: #EC0000; margin: 5px;">President</p></footer></li>
               				</ul>
@@ -58,7 +58,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Jeanette Kootin-Sanwu</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
                 				<li><b>Major:</b> Information Technology</li>
-								<li><b>Email:</b> vicepresident.ucnsbe@gmail.com</li>
+								<li><b>Email:</b><a href="mailto:vicepresident.ucnsbe@gmail.com"> vicepresident.ucnsbe@gmail.com</a></li>
                 				<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>  From Cincinnati, OH. Hobbies include listening to music, watching Netflix, and photography. Fun fact: I’m from Ghana. I hope to increase the exposure and impact of NSBE not just on campus, but throughout the community while continuing to fulfill our mission. </li>
                 				<li><footer><p style="font-weight: 600; text-align: center; margin: 5px;  color: #EC0000">Vice President</p></footer></li>
               				</ul>
@@ -74,7 +74,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Emmanuel Jokotoye</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
                 				<li><b>Major:</b> Computer Engineering</li>
-								<li><b>Email:</b> programs.ucnsbe@gmail.com</li>
+								<li><b>Email:</b><a href="mailto:programs.ucnsbe@gmail.com"> programs.ucnsbe@gmail.com</a></li>
                 				<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>  Originally from Ogbomosho, Nigeria but I grew up in Cincinnati. I enjoy photography, games, learning about new things, listening to music, and exploration. Fun fact about me is I'm bilingual and I am the youngest and only boy in my family. I am very passionate about mentorship, and throughout this year I hope to advice students on how to further their platforms!</li>
                 				<li><footer><p style="font-weight: 600; text-align: center; margin: 5px; color: #EC0000">Programs Chair</p></footer></li>
               				</ul>
@@ -90,7 +90,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Troy Meadows</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
                 				<li><b>Major:</b> Computer Science</li>
-								<li><b>Email:</b> secretary.ucnsbe@gmail.com</li>
+								<li><b>Email:</b><a href="mailto:secretary.ucnsbe@gmail.com"> secretary.ucnsbe@gmail.com</a></li>
                 				<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>I am originally from Columbus, Ohio. My favorite activities include listening to and talking about music, photography, and meeting new people. A fun fact about me is that I own way too many pairs of sneakers. This year, my goal for NSBE is to increase the presence of our chapter on campus.</li>
                 				<li><footer><p style="font-weight: 600; text-align: center; margin: 5px; color: #EC0000">Secretary</p></footer></li>
               				</ul>
@@ -106,7 +106,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Tearra Taylor</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
                 				<li><b>Major:</b> Mechanical Engineering</li>
-								<li><b>Email:</b> treasurer.ucnsbe@gmail.com</li>
+								<li><b>Email:</b><a href="mailto:treasurer.ucnsbe@gmail.com"> treasurer.ucnsbe@gmail.com</a></li>
                 				<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>  I am a Mechanical Engineering Major with a Minor in International Business. I few activities that I enjoy are traveling, exploring (food :) and nature), volunteering, and watching Netflix. For our chapter I plan on properly allocating our funds for all of our events this school year and get us to San Antonio. </li>
                 				<li><footer><p style="font-weight: 600; text-align: center; margin: 5px; color: #EC0000">Treasurer</p></footer></li>
               				</ul>
@@ -122,7 +122,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">FearGod O-Williams</header></li>
                 				<li><b>Year:</b> Junior</li>
                 				<li><b>Major:</b> Biomedical Engineering</li>
-								<li><b>Email:</b> finance.ucnsbe@gmail.com</li>
+								<li><b>Email:</b><a href="mailto:finance.ucnsbe@gmail.com"> finance.ucnsbe@gmail.com</a></li>
                 				<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>I am a 4th year biomedical engineering student, with minors in chemistry and medicine. Really love watching movies, sleeping, and anything...Shonda Rhimes I'm in 100%. This year, as the chapter's finance chair, my goal is to help the chapter establish solid connections (financial and professional) with organizations both in the greater Cincinnati area and around the nation.</li>
                 				<li><footer><p style="font-weight: 600; text-align: center; color: #EC0000; margin: 5px">Finance Chair</p></footer></li>
               				</ul>
@@ -138,7 +138,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Tinashe Tagarisa</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
 				        	<li><b>Major:</b> Computer Science</li>
-							<li><b>Email:</b> membership.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:membership.ucnsbe@gmail.com"> membership.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>Hi, I’m originally from Masvingo, Zimbabwe. I enjoy doing anything creative, and discussing abstract ideas. I hope to encourage all members to share and grow with one another, in order to achieve the potential that we all have. Be sure to say hello if you find me on campus!</li>
                 				<li><footer><p style="font-weight: 600; text-align: center; margin: 5px; color: #EC0000">Membership Chair</p></footer></li>
               				</ul>
@@ -154,7 +154,7 @@
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Knowledge Lacy</header></li>
                 				<li><b>Year:</b> Junior</li>
 				        	<li><b>Major:</b> Computer Engineering</li>
-							<li><b>Email:</b> communication.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:communication.ucnsbe@gmail.com"> communication.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b> From Toledo, OH
 My hobbies include watching and playing basketball, creating Virtual Reality Applications, listening to music, and kicking it with friends.
 I hope to help our chapter grow by using various social media platforms.</li>
@@ -173,7 +173,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Quoran Knights</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
 				        	<li><b>Major:</b> Mechanical Engineering Technology</li>
-							<li><b>Email:</b> academic.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:academic.ucnsbe@gmail.com"> academic.ucnsbe@gmail.com</a></li>
 					      	<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>Originally from Columbus, OH, my hobbies include binge watching TV, hanging with friends, and sleeping. Fun Fact: Engineering has become a second passion, alongside mentoring. I hope to encourage the chapter's members and get them the help that they'd need academically to reach their full potential.</li>
                 				<li><footer><p style="font-size: 18px; font-weight: 600; text-align: center; color: #EC0000; margin: 5px">Academic Excellence Co-Chair</p></footer></li>
               				</ul>
@@ -189,7 +189,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Vincent Copeland</header></li>
                 				<li><b>Year:</b> Sophomore</li>
 				        	<li><b>Major:</b> Chemical Engineering</li>
-							<li><b>Email:</b> service.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:service.ucnsbe@gmail.com"> service.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>Hello, I'm from Cincinnati, OH. My hobbies are listening to music, singing songs, playing video games and hanging out with friends. Fun Fact: Even though I perform a lot, I have bad stage freight every time. I hope to get NSBE more involved in the community and have a better presence in the university and Cincinnati high schools. </li>
                 				<li><footer><p style="margin: 5px; font-size: 18px; font-weight: 600; text-align: center; color: #EC0000">Community Service Co-Chair</p></footer></li>
               				</ul>
@@ -205,7 +205,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Jalyn Stewart</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
 				        	<li><b>Major:</b> Civil Engineering</li>
-							<li><b>Email:</b> service.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:service.ucnsbe@gmail.com"> service.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>Hey, I'm from West Chester, OH. My hobbies include listening to music, finding new podcasts, watching tv and writing. Fun fact: I have a dog named Braxton and he’s the cutest. I plan on doing my best in fulfilling the NSBE mission and I hope to add on more service events on top of what we currently do as a chapter.</li>
                 				<li><footer><p style="font-size: 18px; font-weight: 600; margin: 5px; text-align: center; color: #EC0000">Community Service Co-Chair</p></footer></li>
               				</ul>
@@ -221,7 +221,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Oluwaseyi Oyadiran</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
 				        	<li><b>Major:</b> Environmental Engineering</li>
-							<li><b>Email:</b> cultural.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:cultural.ucnsbe@gmail.com"> cultural.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"> <b>About: </b> I am passionate about creating technology and different innovation to help the environmental and the generation to come. I am the cultural awareness chair for NSBE, but also does more on campus, I am proud Darwin T Turner scholar. and is now Director of student for University of Cincinnati African Student Association where I try to promote and spread African cultures to Campus.</li>
                 				<li><footer><p style="margin: 5px; font-weight: 600; text-align: center; color: #EC0000">Cultural Awareness Chair</p></footer></li>
               				</ul>
@@ -237,7 +237,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Deborah Cole-Taylor</header></li>
                 				<li><b>Year:</b> Sophomore</li>
 				        	<li><b>Major:</b> Chemical Engineering</li>
-							<li><b>Email:</b> pci.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:pci.ucnsbe@gmail.com"> pci.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"> <b>About: </b> I'm from Dayton, OH. My hobbies are roller skating, volunteering, and dancing. Fun fact: I have been playing the flute for 10 years and the saxophone for 8 years. I plan on getting NSBE more involved with Cincinnati youth, and vice versa, and encouraging more students to get excited about pursuing STEM fields by implementing more NSBE Jr. chapters within CPS.</li>
                 				<li><footer><p style="margin: 5px; font-weight: 600; text-align: center; color: #EC0000">PCI Chair</p></footer></li>
               				</ul>
@@ -253,7 +253,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Alexi Wallace</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
 				        	<li><b>Major:</b> Information Technology</li>
-							<li><b>Email:</b> parliamentarian.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:parliamentarian.ucnsbe@gmail.com"> parliamentarian.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"> <b>About: </b>Hi, I'm from Boston, MA. My hobbies include watching Netflix, napping, and traveling. Fun fact: I play for the UC rugby team. I hope to help work with other organizations so that our chapter becomes more involved with the UC community.</li>
                 				<li><footer><p style="margin: 5px; font-weight: 600; text-align: center; color: #EC0000">Parliamentarian</p></footer></li>
               				</ul>
@@ -269,7 +269,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Raphael Hicks</header></li>
                 				<li><b>Year:</b> Sophomore</li>
 				        	<li><b>Major:</b> Chemical Engineering</li>
-							<li><b>Email:</b> senator.ucnsbe@gmail.com</li>
+							<li><b>Email:</b><a href="mailto:senator.ucnsbe@gmail.com"> senator.ucnsbe@gmail.com</a></li>
 				        	<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b>  From Cincinnati, OH. Hobbies: singing, playing video games, watching YouTube and Netflix. Fun fact: I can play 5 instruments: Trombone for 10 years, Percussion for 3 years, Saxophone for 2 years, and Baritone and Trumpet for a year. I plan to represent NSBE in our other engineering organizations and community at large in a way that best reflects and upholds our core values, as well as helping other E-Board members execute their ideas and initiatives. </li>
                 				<li><footer><p style="margin: 5px; font-weight: 600; text-align: center; color: #EC0000">Senator</p></footer></li>
               				</ul>
@@ -285,7 +285,7 @@ I hope to help our chapter grow by using various social media platforms.</li>
                 				<li><header style="text-align: center; font-weight: 600; color: #000000">Sarah Nwaobasi</header></li>
                 				<li><b>Year:</b> Pre-Junior</li>
                 				<li><b>Major:</b> Chemical Engineering</li>
-								<li><b>Email:</b> senator1.ucnsbe@gmail.com</li>
+								<li><b>Email:</b><a href="mailto:senator1.ucnsbe@gmail.com"> senator1.ucnsbe@gmail.com</a></li>
                 				<li style="overflow-y: auto; width: 325px; height: 125px"><b>About: </b> From Hamilton/ West Chester, Ohio. My hobbies include modern west african dance, playing sports, going to the gym, makeup, shopping, and hanging with friends. Fun fact I have gotten stuck in London for 2 days before. I hope to inspire more young underrepresented children to persue a study in STEM.</li>
                 				<li><footer><p style="font-weight: 600; text-align: center; margin: 5px; color: #EC0000">Senator</p></footer></li>
               				</ul>
